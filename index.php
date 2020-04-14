fdab5ce23d317978f926462bce2f2edad1c04078
ynot24 <aaron.govinth@gmail.com>
2020-04-14 15:50:32 +0200

    Create index.php
    Début du site

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon super site</title>
+    </head>
+ 
+    <body>
+ 
+    <?php include("header.php"); ?>
+    
+    <?php include("menu.php"); ?>
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>Mon super site</h1>
+        
+        <p>
+            Bienvenue sur mon super site !<br />
+            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+        </p>
+    </div>
+    
+    <!-- Le pied de page -->
+    
+    <?php include("footer.php"); ?>
+    
+    </body>
+</html>
