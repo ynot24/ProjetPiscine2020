08a84fc2e678b3a9312dd2c73245844c7f5e0e54
ynot24 <aaron.govinth@gmail.com>
2020-04-14 22:44:22 +0200

    Create creation_compte.php
    Partie création de compte

---
+++ creation_compte.php
@@ -0,0 +1,35 @@
+<!-- PARTIE CREATION DE COMPTE -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Création Compte</title>
+    </head>
+ 
+    <body>
+ 
+    <?php include("header.php"); ?>
+    
+    <?php include("menu.php"); ?>
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>CREER UN COMPTE</h1>
+        
+        <p>Bienvenue sur le site d'enchères en ligne de l'ECE Paris !<br />
+        Êtes-vous un vendeur ou un acheteur ? 
+        </p>
+    </div>
+    
+    <p>Si vous êtes vendeur, <a href="creation_compte_vendeur.php">Cliquez ici !</a></p>
+    <br>
+    <p>Si vous êtes acheteur, <a href="creation_compte_acheteur.php">Cliquez ici !</a></p>
+    
+    <!-- Le pied de page -->
+    
+    <?php include("footer.php"); ?>
+    
+    </body>
+</html>
