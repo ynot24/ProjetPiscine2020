712897621a2aac4e498edca25ef3a1a2b89883dd
ynot24 <aaron.govinth@gmail.com>
2020-04-16 13:37:21 +0200

    Create mon_compte.php
    Ajout mon_compte.php dans la branche compte

---
+++ mon_compte.php
@@ -0,0 +1,54 @@
+<?php
+    session_start()
+
+    if($_SESSION['Mail']!="")
+    {
+        ?>
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8" />
+                <title>Mon Compte</title>
+            </head>
+        
+            <body>
+        
+            <?php include("header.php"); ?>
+            
+            <?php include("menu.php"); ?>
+            
+            <!-- Le corps -->
+            
+            <div id="corps">
+                <h1>Mon compte</h1>
+                <div>
+                    <h2>Mes achats</h2>
+                </div>
+                <div>
+                    <h2>Mes ventes</h2>
+                </div>    
+            </div>
+
+            <form action="deconnexion.php">
+                <input type="submit" value="Déconnexion" onclick="msg()">
+            </form>
+
+            <script>
+            function msg() 
+            {
+                alert("Déconnexion réussie !");
+            }
+            </script>
+            
+            <!-- Le pied de page -->
+            
+            <?php include("footer.php"); ?>
+            
+            </body>
+        </html><?php
+    }
+    else
+    {
+        header ("Refresh: 3;URL=PageAccueil.html");
+    }
+?>
