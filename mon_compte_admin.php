f405a1725d80f26519617f76e3962213832e263a
ynot24 <aaron.govinth@gmail.com>
2020-04-17 19:13:30 +0200

    Update mon_compte_admin.php
    Page Mon compte admin à jour

--- mon_compte_admin.php
+++ mon_compte_admin.php
@@ -21,8 +21,18 @@
             
             <div id="corps">
                 <h1>Mon Compte Admin</h1>
-                <h2>PARTIE VENDEUR</h2>
-                <h2>PARTIE ITEM</h2>
+                <div>
+                    <h2>PARTIE VENDEUR</h2>
+                    <a href="liste_vendeur.html">Voir la liste des vendeurs</a>
+                    <a href="creation_compte_vendeur.php">Ajouter un vendeur</a>
+                    <a href="supprimer_vendeur.html">Supprimer un vendeur</a>
+                </div>
+                <div>
+                    <h2>PARTIE ITEM</h2>
+                    <a href="liste_item.html">Voir la liste des items</a>
+                    <a href="vendre_item.php">Ajouter un item</a>
+                    <a href="supprimer_item.html">Supprimer un item</a>
+                </div>
             </div>
 
             <form action="deconnexion.php">
